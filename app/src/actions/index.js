7f321b97cc54ef272d25c1ec1426684fe30d717e
Hussain Ali <ali.415code@gmail.com>
2021-01-20 15:41:16 -0800

    completed action

---
+++ app/src/actions/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export const GET_CAT_START = "GET_CAT_START"
+export const GET_CAT_SUCCESS = "GET_CAT_SUCCESS"
+export const GET_CAT_FAILURE = "GET_CAT_FAILURE"
+
+export const getCat = () => {
+    return (dispatch) => {
+        dispatch({ type: GET_CAT_START})
+
+        axios
+        .get('https://api.thecatapi.com/v1/images/search')
+        .then(res => {
+            console.log(res.data)
+            dispatch({type: GET_CAT_SUCCESS, payload:res.data})
+        })
+        .catch(err => {
+            console.log(err)
+            dispatch({type: GET_CAT_FAILURE, payload: err.message})
+        })
+    }   
+}
+
---
+++ app/src/components/CatCards.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CatCards = (props) => {
+
+    return (
+        <div>
+            <h2>Check</h2>
+        </div>
+    )
+}
+
+export default CatCards
