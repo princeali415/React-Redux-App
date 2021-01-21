44b2e9d61e3ecaabfefd1ed01e6cee1ce7f12f79
Hussain Ali <ali.415code@gmail.com>
2021-01-20 16:10:52 -0800

    created store and applied middleware

--- app/src/index.js
+++ app/src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { reducer } from './reducer/index';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
+
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
